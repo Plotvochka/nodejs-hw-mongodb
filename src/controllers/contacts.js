086f28068172fbff0ae527bb6e385a6cb5f59634
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-10-30 13:55:42 +0200

    done half

---
+++ src/controllers/contacts.js
@@ -0,0 +1,30 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res, next) => {
+  const contacts = await getAllContacts();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  // Відповідь, якщо контакт не знайдено
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  // Відповідь, якщо контакт знайдено
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
